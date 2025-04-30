23f65e3b3e042320324e6701836bd6ebb7360432
Bala-Saravanan <jbalasaravanan174@gmail.com>
2025-04-30 17:43:51 +0530

    FIX: Resolving production URL issues

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ const allowedOrigins = [
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin)) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
